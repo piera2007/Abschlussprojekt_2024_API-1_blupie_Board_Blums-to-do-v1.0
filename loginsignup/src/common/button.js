b60005fb7ef8526f62c592a9dbbf94cff5542a83
Piera <pieramblum@gmail.com>
2024-06-26 16:16:21 +0200

    noch die letzten kommentare hinzugefügt

--- loginsignup/src/common/button.js
+++ loginsignup/src/common/button.js
@@ -1,12 +1,18 @@
+/** 
+  Author: Piera Blum
+  Date: 26.06.2024
+  Description: This is the Styling for the button.
+*/
+
 module.exports = {
-    button1: {
-        backgroundColor: '#F50057',
-        color: 'white',
-        padding: 10,
-        borderRadius: 5,
-        fontSize: 20,
-        minWidth: 150,
-        textAlign: 'center',
-        margin: 10,
-    },
-}
+  button1: {
+    backgroundColor: "#F50057",
+    color: "white",
+    padding: 10,
+    borderRadius: 5,
+    fontSize: 20,
+    minWidth: 150,
+    textAlign: "center",
+    margin: 10,
+  },
+};
